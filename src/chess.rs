4e5bb3aed3344279674553fdbb481d3911939d0b
BoredToad <maxim.chapkanski@gmail.com>
2024-08-20 19:44:30 +0200

    worked a bit on cleaning things up and getting available moves for pawns

--- src/chess.rs
+++ src/chess.rs
@@ -2,22 +2,34 @@ use std::array;
 
 use crate::pieces::*;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum ChessColor {
     White,
     Black,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Coordinate {
-    x: usize,
-    y: usize,
+    pub x: i8,
+    pub y: i8,
 }
+
 impl Coordinate {
-    pub fn tuple(&self) -> (usize, usize) {
+    pub fn tuple(&self) -> (i8, i8) {
         (self.x, self.y)
     }
-    pub fn board_index(&self) -> (isize, isize) {
-        (self.x as isize, self.y as isize)
+    pub fn in_bounds(&self) -> bool {
+        if self.x > BOARD_SIZE as i8 || self.x < 0 || self.y > BOARD_SIZE as i8 || self.y < 0 {
+            false
+        } else {
+            true
+        }
     }
+}
+
+// honestly should have been a macro lol
+pub fn cord(x: i8, y: i8) -> Coordinate {
+    Coordinate { x, y }
 }
 
 #[derive(Debug)]
@@ -25,21 +37,29 @@ pub enum BoardIndexError {
     NegativeIndex,
     LargeIndex,
 }
+
+pub const BOARD_SIZE: usize = 8;
 // board[y][x]
 pub type Square = Option<Box<dyn Piece>>;
-pub struct Board([[Square; 8]; 8]);
+// TODO: make the array field private so I can abstract shidd away later
+pub struct Board(pub [[Square; BOARD_SIZE]; BOARD_SIZE]);
 impl Board {
     pub fn init() -> Board {
         // gonna have to implement the correct starting board at some point
         Board(array::from_fn(|_| array::from_fn(|_| None)))
     }
 
-    pub fn at(&mut self, (x, y): (isize, isize)) -> Result<&mut Square, BoardIndexError> {
-        if x < 0 || y < 0 {
-            return Err(BoardIndexError::NegativeIndex);
+    // NOTE: maybe unneeded
+    pub fn immutable_at(&self, (x, y): (i8, i8)) -> Result<&Square, &str> {
+        if !cord(x, y).in_bounds() {
+            return Err("out of bounds");
         }
-        if x >= 8 || y >= 8 {
-            return Err(BoardIndexError::LargeIndex);
+        Ok(&self.0[x as usize][y as usize])
+    }
+
+    pub fn at(&mut self, (x, y): (i8, i8)) -> Result<&mut Square, &str> {
+        if !cord(x, y).in_bounds() {
+            return Err("out of bounds");
         }
         Ok(&mut self.0[x as usize][y as usize])
     }
--- src/pieces.rs
+++ src/pieces.rs
@@ -18,6 +18,7 @@ struct Pawn {
     has_moved: bool,
 }
 
+// remember to add promotion some day
 impl Piece for Pawn {
     fn new(color: ChessColor, pos: Coordinate) -> Self
     where
@@ -33,24 +34,70 @@ impl Piece for Pawn {
     fn get_color(&self) -> &ChessColor {
         &self.color
     }
+
     fn get_moves(&self, board: &Board) -> Vec<Coordinate> {
-        vec![]
-    }
-    fn move_piece(&mut self, board: &mut Board, pos: Coordinate) -> Result<Square, PieceMoveError> {
-        let square = match board.at(pos.board_index()) {
-            Ok(i) => i,
-            Err(e) => return Err(PieceMoveError::IndexError(e)),
+        let mut possible = vec![];
+        let direction = match self.color {
+            ChessColor::White => 1,
+            ChessColor::Black => -1,
         };
-        self.pos = pos;
-        square = self;
+
+        let first = cord(self.pos.x, self.pos.y + direction);
+        if first.in_bounds() {
+            possible.push(first);
+            let second = cord(self.pos.x, self.pos.y + direction * 2);
+            if second.in_bounds() {
+                possible.push(second)
+            }
+        // this check skips the useless checks for capturing pieces out of bounds
+        } else {
+            return possible;
+        }
+
+        // capturing
+        for side in [-1, 1] {
+            let target = cord(self.pos.x + direction, self.pos.y + side);
+
+            match board.immutable_at(target.tuple()) {
+                Err(_) => continue,
+                Ok(square) => match square {
+                    None => continue,
+                    Some(piece) => {
+                        if self.color == *piece.get_color() {
+                            continue;
+                        }
+                    }
+                },
+            }
+            possible.push(target)
+        }
+
+        possible
+    }
+
+    fn move_piece(&mut self, board: &mut Board, pos: Coordinate) -> Result<Square, PieceMoveError> {
         todo!()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::array;
+
+    use super::*;
+
     #[test]
-    fn idfk() {
-        assert!(false)
+    fn pawn_unmoved_no_obstacles() {
+        let mut board = Board::init();
+        board.0[3][3] = Some(Box::new(Pawn::new(ChessColor::White, cord(3, 3))));
+        board.0[4][4] = Some(Box::new(Pawn::new(ChessColor::Black, cord(4, 4))));
+        let pawn = match &board.0[3][3] {
+            Some(i) => i,
+            _ => panic!("Pawn is None"),
+        };
+        assert_eq!(
+            pawn.get_moves(&board),
+            vec![cord(3, 4), cord(3, 5), cord(4, 4)]
+        );
     }
 }
